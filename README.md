f51dc6501f4b598cb2fcd2c6aafeefd11a770655
veena070898 <74173347+veena070898@users.noreply.github.com>
2020-11-27 16:27:32 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# 22
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,29 @@
+const Engine=Matter.Engine;
+const World=Matter.World;
+const Bodies=Matter.Bodies;
+
+var engine,world;
+var object;
+
+
+
+function setup() {
+ var canvas= createCanvas(400,400);
+ engine=Engine.create();
+ world=engine.world;
+
+var obj={
+  isStatic:true
+}
+ object=Bodies.rectangle(200,100,50,50,obj);
+ World.add(world,object);
+  console.log(object);
+}
+
+function draw() {
+  background(0);
+  Engine.update(engine);
+  rectMode(CENTER);  
+  rect(object.position.x,object.position.y,50,50);
+
+}
